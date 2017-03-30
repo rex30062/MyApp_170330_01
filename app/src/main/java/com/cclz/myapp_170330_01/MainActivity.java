82e0962b402bbf2631a36f526ed842a5d70a7ef3
Cclz <rex620620>
2017-03-30 10:33:37 +0800

    OnItemClickListener

--- app/src/main/java/com/cclz/myapp_170330_01/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170330_01/MainActivity.java
@@ -2,8 +2,12 @@ package com.cclz.myapp_170330_01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv;
@@ -16,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_expandable_list_item_1, fruits);
         lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, fruits[position], Toast.LENGTH_SHORT).show();
+            }   // 秀出點擊的選項
+        });
     }
+
 }
